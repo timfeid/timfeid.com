9572e59a3ca2875527c7a8c847ed1cb2a491312a
Tim Feid <tim@timfeid.com>
2025-02-16 14:46:04 -0500

    chore: lol

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -45,34 +45,38 @@
 			<h1 class="mb-3 mb-6 text-center text-3xl font-bold uppercase">Projects</h1>
 			<div class="grid w-full grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
 				{#each data.posts as post}
-					<a
-						href="/posts/{post.slug}"
-						class="group/post inline-block transition hover:drop-shadow-[0_5px_5px_rgba(0,0,0,0.05)] dark:hover:drop-shadow-[0_5px_5px_rgba(255,255,255,0.05)]"
-					>
-						<Card class="flex h-full flex-col overflow-hidden group-hover/post:border-neutral-700">
-							<CardHeader class="p-0">
-								<img
-									src={post.metadata.image || '/placeholder.svg'}
-									alt={post.metadata.title}
-									class="aspect-video h-48 w-full object-cover"
-								/>
-							</CardHeader>
-							<CardContent class="p-3">
-								<CardTitle>{post.metadata.title}</CardTitle>
-								<CardDescription
-									class="text-neutral-400 transition group-hover/post:text-neutral-300"
-									>{post.metadata.description}</CardDescription
-								>
-							</CardContent>
-							<CardFooter class="mt-auto flex flex-wrap gap-2 p-3">
-								{#each post.metadata.tags as tag}
-									<Badge variant="default">
-										{tag}
-									</Badge>
-								{/each}
-							</CardFooter>
-						</Card>
-					</a>
+					{#if post.metadata.category === 'Projects'}
+						<a
+							href="/posts/{post.slug}"
+							class="group/post inline-block transition hover:drop-shadow-[0_5px_5px_rgba(0,0,0,0.05)] dark:hover:drop-shadow-[0_5px_5px_rgba(255,255,255,0.05)]"
+						>
+							<Card
+								class="flex h-full flex-col overflow-hidden group-hover/post:border-neutral-700"
+							>
+								<CardHeader class="p-0">
+									<img
+										src={post.metadata.image || '/placeholder.svg'}
+										alt={post.metadata.title}
+										class="aspect-video h-48 w-full object-cover"
+									/>
+								</CardHeader>
+								<CardContent class="p-3">
+									<CardTitle>{post.metadata.title}</CardTitle>
+									<CardDescription
+										class="text-neutral-400 transition group-hover/post:text-neutral-300"
+										>{post.metadata.description}</CardDescription
+									>
+								</CardContent>
+								<CardFooter class="mt-auto flex flex-wrap gap-2 p-3">
+									{#each post.metadata.tags as tag}
+										<Badge variant="default">
+											{tag}
+										</Badge>
+									{/each}
+								</CardFooter>
+							</Card>
+						</a>
+					{/if}
 				{/each}
 			</div>
 		</section>
